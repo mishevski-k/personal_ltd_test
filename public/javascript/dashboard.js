a991ee937a995cccf8db1364f8fee8823fde19e5
mishevski-k <mishevski0800@gmail.com>
2021-10-21 20:50:19 +0200

    Added comments in dashboard.js

--- public/javascript/dashboard.js
+++ public/javascript/dashboard.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
+    //No resubmision
     if ( window.history.replaceState ) {  
         window.history.replaceState( null, null, window.location.href );
     }
     
+    //page number for our home page
     let page_number = 0;
 
+        //ajax call to get and display calls data in our calls-container
         $.ajax({
             url: "admin/calls",
             method: "POST",
