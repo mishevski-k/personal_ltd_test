479df8dea34999529db92f5f47c74bcf916e7b46
mishevski-k <mishevski0800@gmail.com>
2021-10-21 20:48:37 +0200

    added comments in main.js

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
 
+    //NO form resubmision
     if ( window.history.replaceState ) {  
         window.history.replaceState( null, null, window.location.href );
     }
 
+    //navbar hamburger menu
     $(".nav-menu").click(function(){
         if($(".nav-links").css("display") === "none") {
             $(".nav-links").css('display', 'block');
@@ -22,10 +24,12 @@ $(document).ready(function(){
         }
     })
 
+    //if no files is selected we will print nothing selected
     if(!$("#import-csv").val()){
         $(".selected-file").html("Nothing selected")
     }
 
+    //Printing the name of the file if we selected one
     $("#import-csv").change(function(){
         console.log()
         $(".selected-file").html($("#import-csv").val().split('\\').pop())
